b7b5e6874beeccc187306c639a19cf4bcbce1977
AndiDomi <1988.domi@gmail.com>
2019-02-05 16:50:24 +0200

    reform

--- Receive.py
+++ Receive.py
@@ -7,6 +7,9 @@ sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 
+def divide_counter_packet():
+    print('yap')
+
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     print ("received message:", data)
--- test.py
+++ test.py
@@ -1,13 +1,19 @@
-import Send
+from Send import add_counter_packet
+from Receive import divide_counter_packet
 import unittest
 
-class TestStringMethods(unittest.TestCase):
-    def test_upper(self):
-        data = bytes('andi', 'ascii')
-        counter=100
-        assert_true='andid'.encode('ascii')
-        self.assertEqual(Send.add_counter_packet(data, counter),assert_true )
+class TestSend(unittest.TestCase):
 
+    def setUp(cls):
+        cls.counter=100
+
+
+    def test_add_counter_packet(self):
+        self.assertEqual(add_counter_packet(bytes('andi', 'ascii'), self.counter), 'andid'.encode('ascii'))
+
+class TestReceive(unittest.TestCase):
+    def test_divide_counter_packet(self):
+        print('failed')
 
 
 if __name__ == '__main__':
